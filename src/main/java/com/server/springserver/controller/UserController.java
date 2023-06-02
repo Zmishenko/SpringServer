e01c45cf7ed3a32cc1b9c26fcbe261312c4df82f
Dos <saxovskij200174@gmail.com>
2023-06-02 22:58:34 +0300

    UPD: проверка и возврат пользователя по запросу

--- src/main/java/com/server/springserver/controller/UserController.java
+++ src/main/java/com/server/springserver/controller/UserController.java
@@ -1,20 +1,37 @@
 package com.server.springserver.controller;
 
+import javax.validation.Valid;
 import com.server.springserver.model.user.User;
 import com.server.springserver.model.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
+    @PostMapping("/authentication/login")
+    public User login(@Valid @RequestBody User user) {
+        user.setAuthorized(false);
+        List<User> users = userService.findAll();
 
-    @GetMapping("/authentication/{login}/{password}")
-    public User getUserById(@PathVariable String login, @PathVariable String password) {
-        return userService.getUserByName(login, password);
-    }
+        for (User userFromBD: users) {
+            if (userFromBD.getPassword().equals(user.getPassword()) && userFromBD.getPassword().equals(user.getPassword())) {
+                user.setAuthorized(true);
+                return userFromBD;
+            }
+        }
+
+        return user;
+
+
+//    @GetMapping("/authentication/{login}/{password}")
+//    public User getUserById(@PathVariable String login, @PathVariable String password) {
+//        return userService.getUser(login, password);
+//    }
+
+}
 }
--- src/main/java/com/server/springserver/model/user/UserService.java
+++ src/main/java/com/server/springserver/model/user/UserService.java
@@ -1,8 +1,9 @@
 package com.server.springserver.model.user;
 
-import com.server.springserver.model.event.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -10,10 +11,14 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public User getUserByName(String login, String password) {
+    public User getUser(String login, String password) {
         User user = repository.findByLogin(login);
         user.setAuthorized(user.getPassword().equals(password));
 
         return user;
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
     }
 }
